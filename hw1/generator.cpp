c29cd946eb29f8be3ac4bb3b48d007670b843f86
BlueBug12 <benson.liu.himself@gmail.com>
2020-04-11 11:35:05 +0800

    arg version

--- hw1/generator.cpp
+++ hw1/generator.cpp
@@ -5,24 +5,25 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char**argv){
 	srand(time(NULL));
 	fstream file;
-	int list[100000];
+	int size = atoi(argv[1]);
+	int list[size];
 	file.open("input.text",ios::out);
 	if(!file){
 		cout<<"Can not open file!"<<endl;
 		exit(1);
 	}
-	for(int i=0;i<100000;++i){
+	for(int i=0;i<size;++i){
 		list[i]=i;
 	}
-	for(int i=0;i<100000;++i){
-		int j=rand()%100000;
+	for(int i=0;i<size;++i){
+		int j=rand()%size;
 		swap(list[i],list[j]);
 	}
-	file<<100000<<endl;
-	for(int i=0;i<100000;++i){
+	file<<size<<endl;
+	for(int i=0;i<size;++i){
 		file<<list[i]<<endl;
 	}
 	file.close();
