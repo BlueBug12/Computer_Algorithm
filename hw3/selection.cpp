a30e18a24cf1258d0463631e26af2a2ee6d34d4a
BlueBug12 <benson.liu.himself@gmail.com>
2020-04-21 18:10:18 +0800

    After debugging

--- hw3/selection.cpp
+++ hw3/selection.cpp
@@ -8,12 +8,14 @@ int* read_file(const char* file_name, int* size, int* rank){
 	file.open(file_name,ios::in);
 	if(!file){
 		cout<<"Can not open input file!"<<endl;
+    exit(1);
 	}
 	string input;
 	file>>input;
 	*size = stoi(input);
   file>>input;
   *rank = stoi(input);
+
 	int* list = new int[*size];
 	for(int i=0;i<*size;++i){
 		file>>input;
@@ -23,8 +25,6 @@ int* read_file(const char* file_name, int* size, int* rank){
 }
 
 int insertion_sort(int* list, const int size){
-  //if(size==1)
-    //return list[0];
 	int key,j;
 	for(int i=1;i<size;++i){
 		key = list[i];
@@ -35,74 +35,76 @@ int insertion_sort(int* list, const int size){
 		}
 		list[j+1]=key;
 	}
-
-  //return list[size/2-size%2];
   return list[size/2];//return the median of list
 }
-int find_median(int* list,int* median_list, const int size){
+int find_median(int* list, const int size){
 
   if(size==1){return list[0];}
-
-
+	int median_size = size/5+(size%5)%2;
+	int* median_list=new int[median_size];
   for(int i=0;i<size;i+=5){
-    if(size-i<5)
-      median_list[i/5]=insertion_sort(list+i,size-i);
+    if((size-i)<5)
+			median_list[i/5]=insertion_sort(list+i,size-i);
+
     else
-      median_list[i/5]=insertion_sort(list+i,5);
+			median_list[i/5]=insertion_sort(list+i,5);
   }
 
-  if(size/5<=1)
-    return median_list[0];
-  else
-    return find_median(median_list,median_list,size/5+(size%5)%2);
+	int target;
+  if(median_size<=5){
+		target=insertion_sort(median_list,median_size);
+	}
+  else{
+		target=find_median(median_list,median_size);
+	}
+	delete[] median_list;
+	return target;
 }
 int find_index(int* list,const int size,const int median){
     for (int i = 0; i <size; ++i){
         if (list[i] == median)
             return i;
     }
+		cout<<"Can't find the index !"<<endl;
     return -1;
 }
-int selection(int* list,int* median_list,int left, int right, int k){
-
-    int median = find_median(list,median_list,left-right+1);
-
-    int index = find_index(list,left-right+1,median);
-    swap(list[index], list[right]);
+int selection(int* list,int size, int rank){
+    int median = find_median(list,size);
+    int index = find_index(list,size,median);
+    //before partion, swap the pivot element and the left element
+    swap(list[index], list[0]);
     //partion
-    int pivot = list[right];
-    int i = left;
-    int j = right - 1;
-    while (true){
-        while(list[i] < pivot)
-            i++;
-        while(list[j] > pivot)
-            j--;
-        if (i < j)
-            swap(list[i], list[j]);
-        else
-            break;
-    }
-    swap(list[i], list[right]);
-    int m = i - left + 1;
+    int pivot = list[0];
+    int i = 0;
+    int j = size;
+    do{
+      do i++;while(list[i]<pivot);
+      do j--;while(list[j]>pivot);
+      if(i<j)swap(list[i],list[j]);
+    }while(i<j);
+    swap(list[0],list[j]);
 
-    if (m == k)
-        return list[i];
-    else if(m > k)
-        return selection(list,median_list, left, i - 1, k);
+    if (j == rank)
+        return list[j];
+    else if(j > rank)
+        return selection(list,j,rank);
     else
-        return selection(list,median_list, i + 1, right, k - m);
+        return selection(list+j+1,size-j-1,rank-j-1);
 }
 
 int main(int argc, char **argv){
 
-  if(argc!=2){cout<<"Wrong input parameters!"<<endl;}
+  if(argc!=2){cout<<"Wrong input parameters!"<<endl;exit(1);}
   int size,rank;
   int* list = read_file(argv[1],&size,&rank);
-  int* median_list = new int[size/5+1];
-  int i = selection(list,median_list, 0, size-1, rank);
+
+  if(rank>size){
+    cout<<"Invalid rank!"<<endl;
+    return 1;
+  }
+  int i = selection(list,size,--rank);
 
   cout << i << endl;
-  delete[] median_list;
+  delete[] list;
   return 0;
 }
